fc38a5c3ee9b430f1156eab9296ef7903e582d35
Rbn <rubenavalosmtz@gmail.com>
2024-04-22 08:44:20 -0600

    bool gameended added

--- Assets/Scripts/Interfaces/IGame.cs
+++ Assets/Scripts/Interfaces/IGame.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace CardGame.Interfaces{
     public interface IGame{
         IDealer Dealer{get;}
+        bool GameEnded{get;}
         void StartGame();
         void StartRound();
         void playRound();
