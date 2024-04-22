14eba45d71c4cd1a6dadb23d1a6620f7d78b6ef3
Rbn <rubenavalosmtz@gmail.com>
2024-04-22 10:35:40 -0600

    update

--- Assets/Scripts/Interfaces/IGame.cs
+++ Assets/Scripts/Interfaces/IGame.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 namespace CardGame.Interfaces{
     public interface IGame{
         IDealer Dealer{get;}
+<<<<<<< Updated upstream
         bool GameEnded{get;}
+=======
+        bool EndedGame{get;}
+        void StartGame();
+>>>>>>> Stashed changes
         void StartRound();
-        void playRound();
+        void PlayRound();
         void ShowWinner();
     }
 }
